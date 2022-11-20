5b5ef4736c461521dea8b880d32bc2b6f2bd1426
aeabdulsamad@gmail.com <aeabdulsamad@gmail.com>
2022-11-20 13:50:47 +0800

    update: concat

--- src/renderer/components/Configuration.vue
+++ src/renderer/components/Configuration.vue
@@ -48,7 +48,7 @@ watch(() => props.loadingConfig, (newValue) => {
             'animate-pulse bg-teal-600 bg-opacity-30' : loading && doneLoading
         }" 
         class="flex items-center px-4 py-1.5 space-x-1.5 border-t w-full justify-between">
-        <span :class="disabledConfig ? 'opacity-50' : ''" class="flex items-start space-x-1">
+        <span :class="disabledConfig ? 'opacity-30' : ''" class="flex items-start space-x-1">
             <span>
                 <svg v-if="isAvailable" class="w-4 h-4 text-teal-600 opacity-80" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path></svg>
                 <svg v-else class="w-4 h-4 text-red-600 opacity-80" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
